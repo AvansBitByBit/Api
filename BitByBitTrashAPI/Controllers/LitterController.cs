5cd47233d8fa935ec63a513080384ba2ae598152
Rishyan Syal <karanrishyan@gmail.com>
2025-06-25 13:34:07 +0200

    Revert "Add [Authorize] attribute to LitterController"
    This reverts commit c6e263a68a7fdba1b36fc8f97c9f6980c073cf33.

--- BitByBitTrashAPI/Controllers/LitterController.cs
+++ BitByBitTrashAPI/Controllers/LitterController.cs
@@ -11,7 +11,6 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BitByBitTrashAPI.Controllers
 {
-    [Authorize]
     [ApiController]
     [Route("Litter")]
     public class LitterController : ControllerBase
